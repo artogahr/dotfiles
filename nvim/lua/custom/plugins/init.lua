6a5c7331a0f8cccee3644b5758cdad14f3c14b38
Artoghrul Gahramanli <artogahr@gmail.com>
2024-05-25 18:32:33 +0200

    Add Neotree and Autopair

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -51,4 +51,35 @@ return {
     config = true,
   },
   { 'nvim-tree/nvim-web-devicons' },
+  {
+    'altermo/ultimate-autopair.nvim',
+    event = { 'InsertEnter', 'CmdlineEnter' },
+    branch = 'v0.6', --recommended as each new version will have breaking changes
+    opts = {
+      --Config goes here
+    },
+  },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      require('neo-tree').setup {
+        popup_border_style = 'rounded',
+        window = {
+          mappings = {
+            ['P'] = { 'toggle_preview', config = { use_float = false, use_image_nvim = true } },
+          },
+        },
+      }
+    end,
+    keys = {
+      { '<leader>t', '<cmd>Neotree toggle<cr>', 'NeoTree' },
+    },
+  },
 }
