dba9c561086f5c8277185f99009c34de91c61bfe
Artoghrul Gahramanli <artogahr@gmail.com>
2024-05-25 19:06:21 +0200

    Add bufferline.nvim

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -82,4 +82,42 @@ return {
       { '<leader>t', '<cmd>Neotree toggle<cr>', 'NeoTree' },
     },
   },
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      vim.o.mousemoveevent = true
+      vim.opt.termguicolors = true
+      require('bufferline').setup {
+        options = {
+          hover = {
+            enabled = true,
+            delay = 200,
+            reveal = { 'close' },
+          },
+
+          --- count is an integer representing total count of errors
+          --- level is a string "error" | "warning"
+          --- diagnostics_dict is a dictionary from error level ("error", "warning" or "info")to number of errors for each level.
+          --- this should return a string
+          --- Don't get too fancy as this function will be executed a lot
+          diagnostics = 'nvim_lsp',
+          diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            local icon = level:match 'error' and ' ' or ' '
+            return ' ' .. icon .. count
+          end,
+          separator_style = 'slant',
+          offsets = {
+            {
+              filetype = 'neo-tree',
+              text = 'NeoTree',
+              highlight = 'Directory',
+              separator = true, -- use a "true" to enable the default, or set your own character
+            },
+          },
+        },
+      }
+    end,
+  },
 }
