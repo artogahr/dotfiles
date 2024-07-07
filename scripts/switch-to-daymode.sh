c9770cdda2906412aab3bf708b85abac22939373
Artoghrul Gahramanli <artogahr@gmail.com>
2024-07-07 21:42:15 +0200

    update auto nightmode scripts

--- scripts/switch-to-daymode.sh
+++ scripts/switch-to-daymode.sh
@@ -2,4 +2,6 @@
 kitty +kitten themes --reload-in=all Catppuccin-Latte
 
 # Change neovim theme
-sed -i 's/catppuccin-frappe/catppuccin-latte/g' ~/.dotfiles/nvim/lua/user/colorscheme.lua
+sed -i 's/catppuccin-frappe/catppuccin-latte/g' ~/workplace/dotfiles/nvim/lua/custom/plugins/init.lua
+
+fish -c "yes | fish_config theme save Catppuccin\ Latte"
--- scripts/switch-to-nightmode.sh
+++ scripts/switch-to-nightmode.sh
@@ -2,4 +2,6 @@
 kitty +kitten themes --reload-in=all Catppuccin-Frappe
 
 # Change neovim theme
-sed -i 's/catppuccin-latte/catppuccin-frappe/g' ~/.dotfiles/nvim/lua/user/colorscheme.lua
+sed -i 's/catppuccin-latte/catppuccin-frappe/g' ~/workplace/dotfiles/nvim/lua/custom/plugins/init.lua
+
+fish -c "yes | fish_config theme save Catppuccin\ Frappe"
