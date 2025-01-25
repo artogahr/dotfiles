b2353a5370ea6bb7e3166ac550fd17917b04c8c0
Artoghrul Gahramanli <artogahr@gmail.com>
2025-01-25 22:34:44 +0100

    Add TV on / off scripts

---
+++ scripts/tv-off.sh
@@ -0,0 +1,1 @@
+gnome-monitor-config set -LM DP-2 -t left -m 1920x1080@75.002 -LpM HDMI-1 -m 1920x1080@75.002 -t normal -x 1080 -y 420
---
+++ scripts/tv-on.sh
@@ -0,0 +1,1 @@
+gnome-monitor-config set -LpM HDMI-2 -m 3840x2160@60.000 -s 2
