018f9ade892d20e08465dee89efed4165498eaf8
Arto Gahr <artogahr@gmail.com>
2025-02-18 00:13:27 +0100

    Some fish plugins from awsm fish

--- Configs/fish/.config/fish/config.fish
+++ Configs/fish/.config/fish/config.fish
@@ -5,3 +5,8 @@ end
 zoxide init fish | source
 
 fzf --fish | source
+
+abbr -a b --function projectdo_build
+abbr -a r --function projectdo_run
+abbr -a t --function projectdo_test
+abbr -a p --function projectdo_tool
